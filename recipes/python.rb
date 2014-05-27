ec0684ad37ba07271271fb75657d633ffcdcc780
Ryan Hass <ryan_hass@rapid7.com>
2014-05-27 15:48:23 -0700

    Set UID/GID on pip installs and handle dulwich
    The dulwich dependency for RhodeCode versions 1.7.2 and below is
    constrained to a version which is no longer available in the PyPi
    repositories. We now need to download the release from github as
    a tarball and install it into the virtualenv manually to ensure
    the dependency resolves.
    
    The other issues addressed in this patch are:
    * Set the UID/GID for the pip installs to the rhodecode user/group.
    * Install mercurial before anything else should we need to use
      mercurial to do a pip install.

--- recipes/python.rb
+++ recipes/python.rb
@@ -25,16 +25,19 @@ python_virtualenv node['rhodecode']['virtualenv']['path'] do
   action :create
 end
 
-python_pip 'rhodecode' do
-  version '1.7.1'
+# Install 
+python_pip 'mercurial' do
   virtualenv node['rhodecode']['virtualenv']['path']
-  options '--allow-external'
-  action :install
+  user node['rhodecode']['system']['user']
+  group node['rhodecode']['system']['group']
+  action [:install, :upgrade]
 end
 
 # Install psycopg2 for postgresql support in RhodeCode.
 python_pip 'psycopg2' do
   virtualenv node['rhodecode']['virtualenv']['path']
+  user node['rhodecode']['system']['user']
+  group node['rhodecode']['system']['group']
   action :install
 end
 
@@ -52,20 +55,45 @@ end
 # http://packages.python.org/RhodeCode/setup.html#setting-up-ldap-support
 python_pip 'python-ldap' do
   virtualenv node['rhodecode']['virtualenv']['path']
+  user node['rhodecode']['system']['user']
+  group node['rhodecode']['system']['group']
   action :install
 end
 
 python_pip 'celery' do
   virtualenv node['rhodecode']['virtualenv']['path']
+  user node['rhodecode']['system']['user']
+  group node['rhodecode']['system']['group']
   action :install
 end
 
 python_pip 'kombu' do
   virtualenv node['rhodecode']['virtualenv']['path']
+  user node['rhodecode']['system']['user']
+  group node['rhodecode']['system']['group']
   action :install
 end
 
-python_pip 'mercurial' do
+# The version of dulwich which RhodeCode (=< 1.7.2) depends on is no longer
+# available in pypi. To work around this issue, we need to download the
+# required version and install it manually into the virtual environment.
+remote_file ::File.join(Chef::Config[:file_cache_path], 'dulwich-0.8.7.tar.gz') do
+  source 'https://github.com/jelmer/dulwich/archive/dulwich-0.8.7.tar.gz'
+  checksum '4e85698cb04fd69e56004e952a2f9a1f5756cf65e06174dbd55635d02cdaab66'
+end
+
+execute 'install_old_dulwich' do
+  command "#{::File.join(node['rhodecode']['virtualenv']['path'], 'bin', 'pip')} install \
+           #{::File.join(Chef::Config[:file_cache_path], 'dulwich-0.8.7.tar.gz')}"
+  not_if "#{::File.join(node['rhodecode']['virtualenv']['path'], 'bin', 'pip')} \
+          freeze | grep -q dulwich==0.8.7"
+end
+
+python_pip 'rhodecode' do
+  version '1.7.2'
   virtualenv node['rhodecode']['virtualenv']['path']
+  user node['rhodecode']['system']['user']
+  group node['rhodecode']['system']['group']
   action :install
 end
+
