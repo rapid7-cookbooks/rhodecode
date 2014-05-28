c62004db653ff440d604948236e9cd472571df8b
Ryan Hass <ryan_hass@rapid7.com>
2014-05-28 08:05:56 -0700

    Use paster from virtualenv
    We now prepend the virtualenv path to the paster calls.

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -38,20 +38,20 @@ end
 # release.
 # https://bitbucket.org/marcinkuzminski/rhodecode/issue/507
 # **  BEGIN WORKAROUND **
-execute "paster" do
+execute "setup-rhodecode" do
   user node['rhodecode']['system']['user']
   group node['rhodecode']['system']['group']
-  command "yes | paster setup-rhodecode /var/lib/rhodecode/production.ini -q --user=#{node['rhodecode']['admin']['user']} --password='#{node['rhodecode']['admin']['passwd']}' --email=#{node['rhodecode']['admin']['email']} --repos=#{node['rhodecode']['repo']['path']} && touch /var/lib/rhodecode/configured_by_chef"
+  command "yes | #{::File.join(node['rhodecode']['virtualenv']['path'], 'bin', 'paster')} setup-rhodecode /var/lib/rhodecode/production.ini -q --user=#{node['rhodecode']['admin']['user']} --password='#{node['rhodecode']['admin']['passwd']}' --email=#{node['rhodecode']['admin']['email']} --repos=#{node['rhodecode']['repo']['path']} && touch /var/lib/rhodecode/configured_by_chef"
   # Do not re-run the setup-rhodecode PasteScript if it has successfully completed.
   creates "/var/lib/rhodecode/configured_by_chef"
   action :run
 end
 ## ** END WORKAROUND **
 
-execute "paster" do
+execute "make-rcext" do
   user node['rhodecode']['system']['user']
   group node['rhodecode']['system']['group']
-  command "paster make-rcext /var/lib/rhodecode/production.ini"
+  command "#{::File.join(node['rhodecode']['virtualenv']['path'], 'bin', 'paster')} make-rcext /var/lib/rhodecode/production.ini"
   creates "/var/lib/rhodecode/rcextensions/__init__.py"
   action :run
 end
